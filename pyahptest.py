d1e5da2ce9bb7bf29411cbaf9400ddef71f75596
huhuzhi <huhuzhi@gmail.com>
2018-05-18 15:13:36 +0800

    pyahptest

---
+++ pyahptest.py
@@ -0,0 +1,11 @@
+import json
+from pyahp import parse
+
+with open('model.json') as json_model:
+    # model can also be a python dictionary
+    model = json.load(json_model)
+
+ahp_model = parse(model)
+priorities = ahp_model.get_priorities()
+print(priorities)
+
